be8a89eb2099fdfe1345f9f4ad201de820d19216
Sydor Dmytro <dmytro.sydor.shi.2023@lpnu.ua>
2026-02-24 22:14:58 +0200

    DL: завантаження даних

--- src/data_load.py
+++ src/data_load.py
@@ -3,3 +3,8 @@ from pathlib import Path
 
 
 def getDataFrame(path: Path = "../data/raw/dtp2024public.csv"):
+    path = Path(path)
+    if not path.exists():
+        raise FileNotFoundError("File not found")
+    df = pd.read_csv(path)
+    return df
