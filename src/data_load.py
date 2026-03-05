fab4b3909430766311de4129051d85974fd08333
Sydor Dmytro <dmytro.sydor.shi.2023@lpnu.ua>
2026-03-05 12:07:20 +0200

    changed uv config and data loading process

--- src/data_load.py
+++ src/data_load.py
@@ -2,9 +2,14 @@ import pandas as pd
 from pathlib import Path
 
 
-def getDataFrame(path: Path = "../data/raw/dtp2024public.csv"):
-    path = Path(path)
+def getDataFrame(path:Path = "../data/raw/dtp2024public.csv" ):
+    path = Path(path, sep = ";", on_bad_lines = "skip")
     if not path.exists():
         raise FileNotFoundError("File not found")
-    df = pd.read_csv(path)
+    df = pd.read_csv(
+        path,
+        sep=";",
+        engine="python",
+        na_values=["null"]
+    )
     return df
