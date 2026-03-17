4f9ee48d9b9aa036b09ba6163a714567ce3aed8a
Sydor Dmytro <dmytro.sydor.shi.2023@lpnu.ua>
2026-03-18 00:13:35 +0200

    downloading data from link

--- src/data_load.py
+++ src/data_load.py
@@ -1,15 +1,26 @@
 import pandas as pd
 from pathlib import Path
 
-
-def getDataFrame(path:Path = "../data/raw/dtp2024public.csv" ):
-    path = Path(path, sep = ";", on_bad_lines = "skip")
-    if not path.exists():
-        raise FileNotFoundError("File not found")
-    df = pd.read_csv(
-        path,
-        sep=";",
-        engine="python",
-        na_values=["null"]
-    )
+def getDataFrame(path: str = "https://opendata.city-adm.lviv.ua/dataset/d07c92fd-dbd9-436a-ac00-8d455c438eb6/resource/377dacf2-e372-4b89-91a8-1911e21e26c3/download/dtp2024public.csv"):
+    # Перевіряємо, чи це посилання, чи локальний шлях
+    if path.startswith("http"):
+        print(f"Завантаження даних з URL: {path}")
+        df = pd.read_csv(
+            path,
+            sep=";",
+            engine="python",
+            on_bad_lines="skip",
+            na_values=["null"]
+        )
+    else:
+        local_path = Path(path)
+        if not local_path.exists():
+            raise FileNotFoundError(f"Файл не знайдено за шляхом: {local_path}")
+        df = pd.read_csv(
+            local_path,
+            sep=";",
+            engine="python",
+            on_bad_lines="skip",
+            na_values=["null"]
+        )
     return df
