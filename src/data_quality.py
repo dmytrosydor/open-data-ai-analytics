750b86403b8986f5c2fbfd50c39309f98da9d109
Sydor Dmytro <dmytro.sydor.shi.2023@lpnu.ua>
2026-03-18 01:42:12 +0200

    macro change to local runner

--- src/data_quality.py
+++ src/data_quality.py
@@ -4,9 +4,9 @@ import pandas as pd
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     print(f"Початкова кількість записів: {df.shape[0]}")
 
-    df['locationComments'] = df['locationComments'].fillna('Коментар відсутній')
+    df["locationComments"] = df["locationComments"].fillna("Коментар відсутній")
 
-    df_cleaned = df.dropna(subset=['latitude', 'longitude'])
+    df_cleaned = df.dropna(subset=["latitude", "longitude"])
 
     print(f"Кількість записів після очищення: {df_cleaned.shape[0]}")
     return df_cleaned
